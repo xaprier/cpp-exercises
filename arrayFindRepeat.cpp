d3a5b01a272f754fa72ef5b095e7b5d76f04d5d3
Seymen Kalkan <69122856+xaprier@users.noreply.github.com>
2022-04-10 20:41:18 +0300

    Recode for English

--- arrayFindRepeat.cpp
+++ arrayFindRepeat.cpp
@@ -4,31 +4,49 @@ using namespace std;
 
 int main() {
     int N;
-    cout << "N değeri girin: ";
+    cout << "Enter N value: ";
     cin >> N;
-    int *dizi = new int[N];
-    int *dizi2 = new int[N];
+    int *array = new int[N], *array2 = new int[N];
     for (int i = 0; i < N; i++) {
-        cout << "dizi değeri girin: ";
-        cin >> dizi[i];
-        dizi2[i] = -1;
+        cout << "Enter array value: ";
+        cin >> array[i];
+
+        // assignment of all the values for repeat counter
+        array2[i] = -1;
     }
-    int adet;
+    int count;
     for (int i = 0; i < N; i++) {
-        adet = 1;
+        // reset count to 1
+        count = 1;
         for (int j = i + 1; j < N; j++) {
-            if (dizi[i] == dizi[j]) {
-                adet++;
-                dizi2[j] = 0;
+
+            // if the index i value equals to next indexes
+            if (array[i] == array[j]) {
+
+                // increment count by 1
+                count++;
+
+                // assign count to 0 for repeated values after index i
+                array2[j] = 0;
             }
         }
-        if (dizi2[i] != 0) {
-            dizi2[i] = adet;
+
+        // if index i count not equals to 0
+        if (array2[i] != 0) {
+
+            // assign the count to count array
+            array2[i] = count;
         }
     }
+
+    // printing the count of values
     for (int i = 0; i < N; i++) {
-        if (dizi2[i] != 0) {
-            cout << dizi[i] << " sayısı " << dizi2[i] << " kere tekrarlanıyor." << endl;
+
+        // repeated numbers not counted again
+        if (array2[i] != 0) {
+
+            // printing
+            cout << "Number " << array[i] << " repeating " << array2[i] << " times." << endl;
         }
     }
 }
