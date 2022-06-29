1b729cb8fa3ca54bd1328662e1dc605f954e38f4
Seymen Kalkan <69122856+xaprier@users.noreply.github.com>
2022-06-29 18:57:22 +0300

    Recode for English

--- findPrimeNumbers2.cpp
+++ findPrimeNumbers2.cpp
@@ -2,35 +2,39 @@
 using namespace std;
 
 int main() {
-    //Asal sayılar 2, 3, 5 ve 7'ye tam bölünemez(kendileri hariç). Bunu ele alarak asal sayıları ve adedini yazdıran koddur.
-    cout << "Sayi : ";
-    int sayi;
-    cin >> sayi;
-    int bolenadedi;
-    //Asal sayı adedini 0'dan başlatıyoruz
-    int asaladedi = 0;
-    //Asal sayılar 2'den başladığından 2'den başlatıyoruz döngüyü
-    for (int i = 2; i <= sayi; i++) {
-        //Bölen adedi değişkenini 0 olarak atıyoruz ki iç döngüde bölen adedi arttırılırsa i değerini yani sayıyı bastırmasın
-        //Ve her döngü başa aldığında tekrar 0 atansın ki her sayı denenirken bolenadedi değişkeni 0 olsun
-        bolenadedi = 0;
-        //Sayımız 2'ye, 3'e, 5'e, 7'den birisine kalansız bölünüyor mu kontrolü(bölünüyorsa aşağısındaki kodlar çalışır = asal sayı değil kontrolü)
+    /*
+    Prime numbers are not divisible by 2, 3, 5 and 7 (except themselves).
+    According to this feature, it is the code that prints the prime numbers and their number.
+    */
+    cout << "Enter top-limit for print and count: ";
+    int limit;
+    cin >> limit;
+    int divisors;
+    // assign the prime count as 0 for ++
+    int pCount = 0;
+    // Since prime numbers start from 2, we start the loop from 2
+    for (int i = 2; i <= limit; i++) {
+        // We set the divisors variable as 0 so that if the number of divisors is increased in the inner loop, 
+        // it will not suppress the i value, that is, the number. And every time the loop starts, it will be assigned 0 again 
+        // so that the divisors variable is 0 while trying every number.
+        divisors = 0;
+        // Check if our number is divisible by 2, 3, 5, 7 without a remainder (if it is divisible, the codes below work = not prime number check)
         if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-            //Bölünen sayılar 2, 3, 5 veya 7 değil mi kontrolü(Asal sayılar kendisine de tam bölündüğünden bu koşul ile 2,3,5,7 asal sayılarını elemiş olacağız.
+            // Check if the divisible numbers are 2, 3, 5 or 7(Since prime numbers are also divisible by themselves, we will eliminate the prime numbers 2,3,5,7 with this condition)
             if (i != 2 && i != 3 && i != 5 && i != 7) {
-                //Koşul sağlanıyorsa bölen adedini 1 arttır
-                bolenadedi++;
+                // if condition is true increment the value of divisors
+                divisors++;
             }
         }
-        //Eğer sayımızı bölen sayı yoksa kontrolü
-        if (bolenadedi == 0) {
-            //Eğer bölen sayı yoksa sayıyı ekrana bastır
+        // if there is no number divisible by our number
+        if (divisors == 0) {
+            // print the value
             cout << i << " ";
-            //Eğer bölen sayı yoksa asal adedimizi 1 arttır
-            asaladedi++;
+            // increment the count
+            pCount++;
         }
     }
     cout << endl;
-    cout << "1 ile " << sayi << " arasi toplam " << asaladedi << " adet asal sayi var.";
+    cout << "There is " << pCount << " prime numbers between 1 and " << limit << endl;
     return 0;
 }
--- matrixColumAndRowAndDiagonalAddition.cpp
+++ matrixColumAndRowAndDiagonalAddition.cpp
@@ -4,50 +4,65 @@ using namespace std;
 
 int main() {
     int N;
-    cout << "N değeri girin: ";
+    cout << "Enter the square matrix's row and colum number: ";
     cin >> N;
-    int **dizi = new int *[N + 1];
+
+    // create an array with dynamic memory allocation (N + 1 is for the pattern and logic)
+    int **array = new int *[N + 1];
     for (int i = 0; i < N + 1; i++) {
-        dizi[i] = new int[N + 1];
+        array[i] = new int[N + 1];
     }
+
+    // assign 0 to matrix's all the values
     for (int i = 0; i < N + 1; i++) {
         for (int j = 0; j < N + 1; j++) {
-            dizi[i][j] = 0;
+            array[i][j] = 0;
         }
     }
+
+    // handling of assign the random value, or addition of row or addition of column or addition of diagonal
     for (int i = 0; i < N + 1; i++) {
         for (int j = 0; j < N + 1; j++) {
+            // if the number of row "and" colum equal to last row and column(diagonal addition)
             if (i == N && j == N) {
                 for (int k = 0; k < N; k++) {
-                    dizi[i][j] += dizi[k][k];
+                    array[i][j] += array[k][k];
                 }
+            // else if the value of row equal to last row(row addition of column k)
             } else if (i == N) {
                 for (int k = 0; k < N; k++) {
-                    dizi[i][j] += dizi[k][j];
+                    array[i][j] += array[k][j];
                 }
+            // else if the value of column equal to last column(column addition of row k)
             } else if (j == N) {
                 for (int k = 0; k < N; k++) {
-                    dizi[i][j] += dizi[i][k];
+                    array[i][j] += array[i][k];
                 }
+            // else assign a random value between 10-19
             } else {
-                dizi[i][j] = rand() % 10 + 10;
+                array[i][j] = rand() % 10 + 10;
             }
         }
     }
-    int kontrol = 0;
-    cout << "Matris" << endl;
+
+    // print the pattern with special handle for printing
+    int check = 0;
+    cout << "Matrix" << endl;
     for (int i = 0; i < N + 1; i++) {
         for (int j = 0; j < N + 1; j++) {
+            // if the column number is before the last column
             if (j == N - 1) {
-                cout << dizi[i][j] << "\t\t";
+                cout << array[i][j] << "\t\t";
+            // else if the row number equal to last row
             } else if (i == N) {
-                if (j == 0 && kontrol == 0) {
+                // if the column is first "and" the check equal to zero
+                if (j == 0 && check == 0) {
                     cout << endl;
-                    kontrol++;
+                    check++;
                 }
-                cout << dizi[i][j] << "\t";
+                cout << array[i][j] << "\t";
             } else {
-                cout << dizi[i][j] << "\t";
+                cout << array[i][j] << "\t";
             }
         }
         cout << endl;
