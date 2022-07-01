20a4f55fe805a084ba1f4cbfb53798d353b7b812
Seymen Kalkan <seymenkalkan@gmail.com>
2022-07-01 15:03:20 +0300

    Recode for English
    Recoded for English and tested with successfully

--- squareWithArea.cpp
+++ squareWithArea.cpp
@@ -3,35 +3,36 @@
 using namespace std;
 
 int main() {
-    
-    cout << "Karenin kenar uzunluğunu girin: ";
-    int sayi;
-    cin >> sayi;
-    string alan = to_string(sayi*sayi);
-    if (sayi <= 4) {
-        cout << "Girdiğiniz değer en az 5 olmalı";
-    } else if (sayi % 2 == 0) { 
-        cout << "Girdiğiniz değer tek sayı olmalı";
+    cout << "Enter the side length of square: ";
+
+    int length;
+    cin >> length;
+
+    string area = to_string(length*length);
+    if (length <= 4) {
+        cout << "Value must be at least 5";
+    } else if (length % 2 == 0) { 
+        cout << "Value must be an odd integer";
     } else {
-        for (int i = 1; i <= sayi; i++) {
-            if (i == 1 || i == sayi) {
-                for (int i = 1; i <= sayi; i++) {
+        for (int i = 1; i <= length; i++) {
+            if (i == 1 || i == length) {
+                for (int i = 1; i <= length; i++) {
                     cout << "M ";
                 }
-            } else if (i == (sayi / 2) + 1) {
-                for (int k = 1; k <= 2*sayi; k++) {
-                    if (k == sayi-alan.length()+1) {
-                        cout << alan;
+            } else if (i == (length / 2) + 1) {
+                for (int k = 1; k <= 2*length; k++) {
+                    if (k == length-area.length()+1) {
+                        cout << area;
                     } 
-                    if (k == 1 || k == 2*sayi-2-alan.length()) {
+                    if (k == 1 || k == 2*length-2-area.length()) {
                         cout << "M";
                     } 
                     cout << " ";
                 }
             }
             else {
-                for (int j = 1; j <= 2*sayi; j++) {
-                    if (j == 1 || j == 2*sayi-2) {
+                for (int j = 1; j <= 2*length; j++) {
+                    if (j == 1 || j == 2*length-2) {
                         cout << "M";
                     } 
                     cout << " ";
