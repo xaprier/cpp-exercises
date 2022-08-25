af0f4edbc00a56e9976c480c15799ab634bd391e
Seymen Kalkan <seymenkalkan@gmail.com>
2022-08-25 17:04:45 +0300

    * Recode for English * Fixed unrelated file name

--- sortMatrixColumns.cpp
+++ sortMatrixColumns.cpp
@@ -2,42 +2,42 @@
 
 using namespace std;
 
-void sirala(int **ptr, int satir, int sutun);
+void sortTheColumns(int **array, int rows, int columns);
 
 int main() {
     int M, N;
-    cout << "Matrisin satir sayisini giriniz: ";
+    cout << "Enter row number of matrix: ";
     cin >> M;
-    cout << "Matrisin sutun sayisini giriniz: ";
+    cout << "Enter column number of matrix: ";
     cin >> N;
-    int **ptr = new int *[M];
-    for (int a = 0; a < M; a++)
-        ptr[a] = new int[N];
+    int **arr = new int *[M];
+    for (int i = 0; i < M; i++)
+        arr[i] = new int[N];
 
-    cout << "Matris elemanlarini satir satir giriniz: " << endl;
-    for (int b = 0; b < M; b++)
-        for (int c = 0; c < N; c++)
-            cin >> *(*(ptr + b) + c);
+    cout << "Enter the matrix members row by row: " << endl;
+    for (int i = 0; i < M; i++)
+        for (int j = 0; j < N; j++)
+            cin >> *(*(arr + i) + j);
 
-    sirala(ptr, M, N);
+    sortTheColumns(arr, M, N);
 }
-void sirala(int **ptr, int satir, int sutun) {
+void sortTheColumns(int **array, int rows, int columns) {
     int temporary;
-    for (int k = 0; k < satir; k++) {
-        for (int i = 0; i < sutun; i++) {
-            for (int j = 0; j < sutun - 1 - i; j++) {
-                if (ptr[k][j] > ptr[k][j + 1]) {
-                    temporary = ptr[k][j + 1];
-                    ptr[k][j + 1] = ptr[k][j];
-                    ptr[k][j] = temporary;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            for (int k = 0; k < rows - j - 1; k++) {
+                if (*(*(array + k) + i) < *(*(array + (k + 1)) + i)) {
+                    temporary = *(*(array + (k + 1)) + i);
+                    *(*(array + (k + 1)) + i) = *(*(array + k) + i);
+                    *(*(array + k) + i) = temporary;
                 }
             }
         }
     }
-    cout << "Satirlari buyukten kucuge siralanmis matris su sekildedir:" << endl;
-    for (int sayi1 = 0; sayi1 < satir; sayi1++) {
-        for (int sayi2 = 0; sayi2 < sutun; sayi2++) {
-            cout << *(*(ptr + sayi1) + sayi2) << " ";
+    cout << "The matrix with sorted columns:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            cout << *(*(array + i) + j) << " ";
         }
         cout << endl;
     }
